475e166d4ca79ff5feb935f69d8539a2f97e3d05
U1 <U1@DESKTOP-7HF2I9M>
2017-04-16 11:14:12 +0300

    Updated documentations

--- Server/SolveCommand.cs
+++ Server/SolveCommand.cs
@@ -7,13 +7,28 @@ using System.Threading.Tasks;
 
 namespace Server
 {
+    /*
+     * The SolveCommand class.   
+     * The class handles the 'solve' command.
+     * The command ask for a solution by a BFS/DFS
+     * algorithms to a specific maze.
+     */
     public class SolveCommand : Command
     {
+        /*
+         * The SolveCommand constructor.
+         * Inherited by the Command class.
+         */
         public SolveCommand(IModel model) : base(model)
         {
         }
-
-
+        /*
+         * The Execute method.
+         * The method executes a specific command,
+         * according to its implementation, using
+         * relevant parameters, and the sender's details
+         * (Client).
+         */
         public override void Execute(Player client, string parameters)
         {
             throw new NotImplementedException();
