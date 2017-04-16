279d991e372716c277b386800d2e3ccb3339aa08
U1 <U1@DESKTOP-7HF2I9M>
2017-04-16 12:30:24 +0300

    Updated project hirerchy, and testing.

---
+++ Server/Adapters/JasonSolutionBuilder.cs
@@ -0,0 +1,79 @@
+﻿using SearchAlgorithmsLib.Interfaces;
+using SearchAlgorithmsLib;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using MazeLib;
+
+namespace AP2EX1.Adapters
+{
+    public class JasonSolutionBuilder : ISolutionJasonBuilder
+    {
+        private const int LEFT_SYMBOL = 0;
+        private const int RIGHT_SYMBOL = 1;
+        private const int UP_SYMBOL = 2;
+        private const int DOWN_SYMBOL = 3;
+
+        private readonly Solution<Position> _solution;
+        private readonly JObject _jasonObject;
+
+        public JasonSolutionBuilder(Solution<Position> solution)
+        {
+            _solution = solution;
+
+            // To assure properties ordering
+            _jasonObject = new JObject();
+            _jasonObject["Name"] = string.Empty;
+            _jasonObject["Solution"] = GetSolutionStr();
+            _jasonObject["NodesEvaluated"] = string.Empty;
+        }
+
+        private string GetSolutionStr()
+        {
+            var solutionBuilder = new StringBuilder();
+            State<Position> currentState = _solution.States.Pop();
+
+            State<Position> nextState;
+            Direction direction;
+
+            while (_solution.States.Count >= 1)
+            {
+                nextState = _solution.States.Pop();
+                direction = currentState.Value.Subtract(nextState.Value);
+                solutionBuilder.Append(GetDirectionSymbol(direction));
+
+                if (_solution.States.Count != 0)
+                    solutionBuilder.Append(",");
+
+                currentState = nextState;
+            }
+
+            return solutionBuilder.ToString();
+        }
+
+        private string GetDirectionSymbol(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    return UP_SYMBOL.ToString();
+                case Direction.Down:
+                    return DOWN_SYMBOL.ToString();
+                case Direction.Left:
+                    return LEFT_SYMBOL.ToString();
+                default:
+                    return RIGHT_SYMBOL.ToString();
+            }
+        }
+
+        public JToken this[string property]
+        {
+            set { _jasonObject[property] = value; }
+            get { return _jasonObject[property]; }
+        }
+
+        public JObject ToJason()
+        {
+            return _jasonObject;
+        }
+    }
+}
---
+++ Server/Adapters/MazeAdapter.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using SearchAlgorithmsLib;
+using SearchAlgorithmsLib.Interfaces;
+using MazeLib;
+using AP2EX1.Interfaces;
+
+namespace AP2EX1.Adapters
+{
+    class MazeAdapter : ISearchable<Position>,IMaze,IDrawable
+    {
+        private Maze _maze;
+
+        public Solution<Position> Solution { set; get; }
+
+        public MazeAdapter(Maze maze)
+        {
+            _maze = maze;
+            var p = new Position();
+            var c = maze[p.Row, p.Col];
+        }
+
+        private bool TryGetFreeNeighbor(Direction relativeLocation, Position currentPos, out Position neighbor)
+        {
+            int neighborCol = currentPos.Col, neighborRow = currentPos.Row;
+
+            switch (relativeLocation)
+            {
+                case Direction.Left:
+                    neighborCol--;
+                    break;
+                case Direction.Right:
+                    neighborCol++;
+                    break;
+                case Direction.Down:
+                    neighborRow--;
+                    break;
+                case Direction.Up:
+                default:
+                    neighborRow++;
+                    break;
+            }
+
+            neighbor = new Position(neighborRow, neighborCol);
+
+            if (neighborCol < 0 || neighborRow < 0 || 
+                neighborCol == _maze.Cols || neighborRow == _maze.Rows ||
+                _maze[neighborRow,neighborCol]==CellType.Wall)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #region ISearchable interface implementation
+        public List<State<Position>> GetAllPossibleStates(State<Position> state)
+        {
+            //TODO: add came from and cost
+            var neighbors = new List<State<Position>>();
+            var relativeDirections = new List<Direction>{Direction.Up,Direction.Right,Direction.Down,Direction.Left};
+            
+            var currentPosition = state.Value;            
+            var neighbor = new Position();
+
+            foreach (var direction in relativeDirections)
+            {
+                if(TryGetFreeNeighbor(direction,currentPosition,out neighbor))
+                {
+                    neighbors.Add(new State<Position>(neighbor));//ref neighbor));
+                }
+            }
+
+            return neighbors;
+        }
+
+        public State<Position> GetGoalState()
+        {
+            Position goalPosition = _maze.GoalPos;
+            return new State<Position>(goalPosition);//ref goalPosition);
+        }
+
+        public State<Position> GetInitialState()
+        {
+            Position initialPosition = _maze.InitialPos;
+            return new State<Position>(initialPosition);//ref initialPosition);
+        }
+        #endregion
+
+        #region IMaze interface implementation
+        public int Cols
+        {
+            get
+            {
+                return _maze.Cols;
+            }
+        }
+
+        public int Rows
+        {
+            get
+            {
+                return _maze.Rows;
+            }
+        }
+
+        public Position GoalPos
+        {
+            get
+            {
+                return _maze.GoalPos;
+            }
+            set
+            {
+                _maze.GoalPos = value;
+            }
+        }
+
+        public Position InitialPos
+        {
+            get
+            {
+                return _maze.InitialPos;
+            }
+            set
+            {
+                _maze.InitialPos = value;
+            }
+        }
+
+        public CellType this[int row, int col]
+        {
+            get
+            {
+                return _maze[row, col];
+            }
+            set
+            {
+                _maze[row, col] = value;
+            }
+        }
+        #endregion
+
+        #region IDrawable interface implementation
+        public char Draw(int x, int y)
+        {
+            var position = new Position(y,x);
+            switch (_maze[x, y])
+            {
+                case CellType.Free:
+                    if (Solution == null || !Solution.Contains(position))
+                    {
+                        return ' ';
+                    }
+                    else
+                    {
+                        return '.';
+                    }
+                default:
+                case CellType.Wall:
+                    return '*';
+            }
+        }
+
+        #endregion
+    }
+}
