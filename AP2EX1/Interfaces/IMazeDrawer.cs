08f37e4c36250cf0f0d46f925fbe14fe8b021676
AlonUriah <alonuriah@gmail.com>
2017-04-18 21:57:39 +0300

    SearchLib and AP2EX1 Projects
    Searchlib is a Class Library exposes Search algorithms like BFS and DFS
    AP2EX1 holds MazeAdapter and MazeDrawer

---
+++ AP2EX1/Interfaces/IMazeDrawer.cs
@@ -0,0 +1,14 @@
+﻿namespace AP2EX1.Interfaces
+{
+    /// <summary>
+    /// Allowing multiple implementations in future
+    /// </summary>
+    public interface IMazeDrawer
+    {
+        /// <summary>
+        /// Draw a given IMaze
+        /// </summary>
+        /// <param name="maze"></param>
+        void DrawMaze(IMaze maze);
+    }
+}
