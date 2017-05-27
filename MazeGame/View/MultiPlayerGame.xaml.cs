e2fccc5e15259c42b395112874f545b24acf5505
Uriah Ahrak <P0022491@ness-tech.co.il>
2017-05-27 21:26:01 +0300

    Updated multiplayer behvaior + win situation.

--- MazeGame/View/MultiPlayerGame.xaml.cs
+++ MazeGame/View/MultiPlayerGame.xaml.cs
@@ -20,15 +20,68 @@ namespace MazeGame.View
     /// </summary>
     public partial class MultiPlayerGame : Window
     {
-        IMultiPlayerViewModel vm;
-        public MultiPlayerGame()
+        private IMultiPlayerViewModel vm;
+        private Window parent;
+
+        public MultiPlayerGame(IMultiPlayerViewModel vm, Window parent)
         {
             InitializeComponent();
-            // new VM
-            // define datacontext
-            // listen to opponent location
-            // Display Awaiting for opponent logic
-            // 
+            this.vm = vm;
+            this.parent = parent;
+            this.DataContext = this.vm;
+            this.KeyDown += this.GameSurface_KeyDown;
+            this.vm.GameStatusChanged += this.OnStatusChanged;
+        }
+        private void GameSurface_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    this.player.Key = "up";
+                    break;
+                case Key.Down:
+                    this.player.Key = "down";
+                    break;
+                case Key.Right:
+                    this.player.Key = "right";
+                    break;
+                case Key.Left:
+                    this.player.Key = "left";
+                    break;
+                default:
+                    break;
+            }
+            this.vm.PlayerMoved("player", this.player.Key);
+        }
+        private void btn_quit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnStatusChanged(object sender, Nullable<int> status)
+        {
+            switch (status)
+            {
+                case 0:
+                    MessageBox.Show("You win!");
+                    this.Close();
+                    break;
+                case 1:
+                    MessageBox.Show("You lost!");
+                    this.Close();
+                    break;
+                case 2:
+                    MessageBox.Show("Connection lost by one of players.");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void wdw_multi_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.vm.Close();
+            this.parent.Show();
         }
     }
 }
---
+++ MazeGame/View/ValueConverters/NameToGameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MazeGame.View.ValueConverters
+{
+    public class NameToGameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string name = (string)(value);
+            if (name == "player")
+                return "DataContext.PlayerRow";
+            else
+                return "DataContext.PlayerCol";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
